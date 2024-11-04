9d23ed1e0c01a1269f0db9743b42a35caa054f71
Ben Leitch <benleitch17@gmail.com>
2024-11-03 17:59:42 -0800

    Added more Naive Bayes experiments. 50% accuracy with binary classification

--- naive_bayes.py
+++ naive_bayes.py
@@ -4,39 +4,196 @@ from dataset_bci_iv import BciIvCsvParser
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from typing import Callable
 
+# Type aliases
+AccuracyScore = float
+ConfusionMatrix = list[list[int]]
+ClassificationReport = str
 
+# Other constants
 BCI_COMP_DATASET_PATH: str = "dataset-bci-iv-2a/"
 
+# Used as seed for test_train_split
+RANDOM_STATE: int = 17 
+TEST_SIZE: float = 0.2
 
-if __name__ == "__main__":
-    bci_iv_parser: BciIvCsvParser = BciIvCsvParser(f"{BCI_COMP_DATASET_PATH}A01E.csv")
-    
-    df: pd.DataFrame = bci_iv_parser.get_dataframe()
 
-    # Not dealing with EOG data for now
-    df = df.drop(columns=["EOGL", "EOGM", "EOGR"])
+def train_naive_bayes(X: pd.DataFrame, y: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Train a Naive Bayes classifier on the given data and labels.
 
-    print(df.shape)
-    print(df.head())
+    :param X: The data to train on.
+    :param y: The labels to train on.
 
-    artifact_count = df[df["Artifact"] == 1.0].shape[0]
-    print(f"Number of rows with Artifact = 1.0: {artifact_count}")
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
 
-    # Removing artifacts
-    df = df[df["Artifact"] != 1.0]
-    df = df.drop(columns=["Artifact"])
-
-    X = df.drop(columns=["Label"])
-    y = df["Label"]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=17)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
 
     gnb = GaussianNB()
     gnb.fit(X_train, y_train)
 
     y_pred = gnb.predict(X_test)
 
-    print(f"Accuracy: {accuracy_score(y_test, y_pred)}")
-    print(f"Confusion Matrix:\n{confusion_matrix(y_test, y_pred)}")
-    print(f"Classification Report:\n{classification_report(y_test, y_pred)}")
+    gnb_accuracy = accuracy_score(y_test, y_pred)
+    gnb_confusion_matrix = confusion_matrix(y_test, y_pred)
+    gnb_classification_report = classification_report(y_test, y_pred)
+    
+    return gnb_accuracy, gnb_confusion_matrix, gnb_classification_report
+
+
+def experiment_initial(df: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Feed all 22 EEG channels into a Naive Bayes classifier, and attempt to classify 5 MI states:
+    0: Rest
+    1: Left Hand
+    2: Right Hand
+    3: Feet
+    4: Tongue
+    Prints the accuracy to the terminal.
+
+    :param df: The EEG data, with labels - expects all Artifacts to be removed and labels to be in a column called Label.
+
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
+
+    # Separate data from labels
+    X = df.drop(columns=["Label"])
+    y = df["Label"]
+
+    return train_naive_bayes(X, y)
+
+
+def experiment_no_rest(df: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Feed all 22 EEG channels into a Naive Bayes classifier and remove the resting state.
+    Classify 4 MI states:
+    1: Left Hand
+    2: Right Hand
+    3: Feet
+    4: Tongue
+    Prints the accuracy to the terminal.
+
+    :param df: The EEG data, with labels - expects all Artifacts to be removed and labels to be in a column called Label.
+
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
+
+    # Remove the resting state
+    df = df[df["Label"] != 0]
+
+    # Separate data from labels
+    X = df.drop(columns=["Label"])
+    y = df["Label"]
+
+    return train_naive_bayes(X, y)
+
+
+def experiment_5_channel(df: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Feed 5 EEG channels into a Naive Bayes classifier.
+    Classify 5 MI states:
+    0: Rest
+    1: Left Hand
+    2: Right Hand
+    3: Feet
+    4: Tongue
+    Prints the accuracy to the terminal.
+
+    :param df: The EEG data, with labels - expects all Artifacts to be removed and labels to be in a column called Label.
+
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
+
+    # Separate data from labels
+    X = df[["Fz", "C3", "Cz", "C4", "Pz"]]
+    y = df["Label"]
+
+    return train_naive_bayes(X, y)
+
+
+def experiment_5_channel_no_rest(df: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Feed 5 EEG channels into a Naive Bayes classifier and remove the resting state.
+    Classify 4 MI states:
+    1: Left Hand
+    2: Right Hand
+    3: Feet
+    4: Tongue
+    Prints the accuracy to the terminal.
+
+    :param df: The EEG data, with labels - expects all Artifacts to be removed and labels to be in a column called Label.
+
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
+
+    # Remove the resting state
+    df = df[df["Label"] != 0]
+
+    # Separate data from labels
+    X = df[["Fz", "C3", "Cz", "C4", "Pz"]]
+    y = df["Label"]
+
+    return train_naive_bayes(X, y)
+
+
+def experiment_left_right_hand(df: pd.DataFrame) -> tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]:
+    """
+    Feed all 22 EEG channels into a Naive Bayes classifier and remove resetin state, feed, and tongue.
+    Classify 2 MI states:
+    1: Left Hand
+    2: Right Hand
+    Prints the accuracy to the terminal.
+
+    :param df: The EEG data, with labels - expects all Artifacts to be removed and labels to be in a column called Label.
+
+    :return: A tuple containing the accuracy, confusion matrix and classification report.
+    """
+
+    # Extract left and right hand data
+    criterion = df["Label"].map(lambda x: x == 1 or x == 2)
+    df = df[criterion]
+
+    # Separate data from labels
+    X = df.drop(columns=["Label"])
+    y = df["Label"]
+
+    return train_naive_bayes(X, y)
+
+
+if __name__ == "__main__":
+    bci_iv_parser_session_train: BciIvCsvParser = BciIvCsvParser(f"{BCI_COMP_DATASET_PATH}A01T.csv")
+    bci_iv_parser_session_eval: BciIvCsvParser = BciIvCsvParser(f"{BCI_COMP_DATASET_PATH}A01E.csv")    
+
+    df_train: pd.DataFrame = bci_iv_parser_session_train.get_dataframe()
+    df_eval: pd.DataFrame = bci_iv_parser_session_eval.get_dataframe()
+
+    df: pd.DataFrame = pd.concat([df_train, df_eval])
+
+    print(f"Dataframe shape:\n{df.shape}")
+    print(f"First few rows:\n{df.head()}")
+    print(f"Artifact rows:\n{df[df["Artifact"] == 1.0].shape[0]}")
+
+    # Not dealing with EOG data for now
+    df = df.drop(columns=["EOGL", "EOGM", "EOGR"])
+
+    # Removing rejected trials
+    df = df[df["Artifact"] != 1.0]
+    df = df.drop(columns=["Artifact"])
+
+    # There may still be some NaN values from the resting state recording, so remove those rows too.
+    df = df.dropna()
+
+    experiments: list[tuple[str, Callable[[pd.DataFrame], tuple[AccuracyScore, ConfusionMatrix, ClassificationReport]]]] = [
+        ("Initial", experiment_initial),
+        ("No Rest", experiment_no_rest),
+        ("5 Channel", experiment_5_channel),
+        ("5 Channel No Rest", experiment_5_channel_no_rest),
+        ("Left Right Hand", experiment_left_right_hand)
+    ]
+
+    for experiment_name, experiment_function in experiments:
+        print(f"Running experiment: {experiment_name}")
+        accuracy, _, _ = experiment_function(df)
+        print(f"Accuracy: {accuracy}")
