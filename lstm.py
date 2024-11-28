b3467d4e69364a694e1ef0e9274d9b9d6a7f7135
Ben Leitch <benleitch17@gmail.com>
2024-11-27 19:06:05 -0800

    Single train-test split for LSTM

--- lstm.py
+++ lstm.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import pandas as pd
 import numpy as np
@@ -10,7 +11,22 @@ from device import get_system_device
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from sklearn.metrics import precision_score, f1_score, confusion_matrix
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
+
+
+LOG: logging.Logger = logging.getLogger(__name__)
+
+
+def configure_logging(log_level: int) -> None:
+    log_format: str = "%(module)s - %(levelname)s - %(message)s"
+    formatter: logging.Formatter = logging.Formatter(log_format)
+
+    # handler: logging.Handler = logging.StreamHandler(sys.stdout)
+    handler: logging.Handler = logging.FileHandler("lstm.log", mode="w")
+    handler.setFormatter(formatter)
+    
+    LOG.setLevel(log_level)
+    LOG.addHandler(handler)
 
 
 class LSTM(nn.Module):
@@ -64,41 +80,16 @@ class LSTM(nn.Module):
         return x
 
 
-class EarlyStopping:
-    def __init__(self, patience=7, min_delta=0):
-        self.patience = patience
-        self.min_delta = min_delta
-        self.counter = 0
-        self.best_loss = None
-        self.early_stop = False
-        
-    def __call__(self, val_loss):
-        if self.best_loss is None:
-            self.best_loss = val_loss
-
-        elif val_loss > self.best_loss - self.min_delta:
-            self.counter += 1
-            if self.counter >= self.patience:
-                self.early_stop = True
-
-        else:
-            self.best_loss = val_loss
-            self.counter = 0
-
-
-def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler, device, num_epochs=15):
+def train_model(model, train_loader, test_loader, criterion, optimizer, device, num_epochs=20):
     history = {
         'train_loss': [],
         'train_acc': [],
-        'val_loss': [],
-        'val_acc': [],
-        'val_precision': [],
-        'val_f1': [],
-        'val_conf_matrix': [],
-        'lr': [],
+        'test_loss': [],
+        'test_acc': [],
+        'precision': [],
+        'f1': [],
+        'conf_matrix': []
     }
-    early_stopping = EarlyStopping(patience=7, min_delta=0.001)
-    best_val_acc = 0.0
 
     for epoch in tqdm(range(num_epochs)):
         # model should be in training mode
@@ -129,172 +120,127 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler
         avg_train_loss = train_loss / len(train_loader)
         train_accuracy = 100 * train_correct_predictions / train_total_predictions
 
-        # Validation phase - switch model to evaluation mode
-        model.eval()
-
-        val_loss = 0.0
-        val_correct_predictions = 0
-        val_total_predictions = 0
-
-        all_labels = []
-        all_predictions = []
-
-        with torch.no_grad():
-            for inputs, labels in val_loader:
-                inputs, labels = inputs.to(device), labels.to(device)
-
-                outputs = model(inputs.permute(0, 2, 1))
-
-                # Loss calculation
-                loss = criterion(outputs, labels)
-                val_loss += loss.item()
-
-                # Get the predictions
-                _, predicted = torch.max(outputs, 1)
-
-                # Collect all the predictions made 
-                all_labels.extend(labels.cpu().numpy())
-                all_predictions.extend(predicted.cpu().numpy())
-
-        # Average loss and accuracy:
-        val_total_predictions = len(all_labels)
-        val_correct_predictions = sum([1 for i in range(val_total_predictions) if all_labels[i] == all_predictions[i]])
-
-        avg_val_loss = val_loss / len(val_loader)
-        val_accuracy = 100 * val_correct_predictions / val_total_predictions
-
-        # Addional metrics: precision, f1-score, confusion matrix
-        precision = precision_score(all_labels, all_predictions, average='macro')
-        f1 = f1_score(all_labels, all_predictions, average='macro')
-        conf_matrix = confusion_matrix(all_labels, all_predictions)
-
-        # Record metrics
+        # Record training metrics
         history['train_loss'].append(avg_train_loss)
         history['train_acc'].append(train_accuracy)
-        history['val_loss'].append(avg_val_loss)
-        history['val_acc'].append(val_accuracy)
-        history['val_precision'].append(precision)
-        history['val_f1'].append(f1)
-        history['val_conf_matrix'].append(conf_matrix)
 
-        print(f'\nEpoch {epoch + 1} / {num_epochs}:')
-        print(f'Train Loss: {avg_train_loss:.3f}, Train Acc: {train_accuracy:.2f}%')
-        print(f'Val Loss: {avg_val_loss:.3f}, Val Acc: {val_accuracy:.2f}%')
+        LOG.debug(f'\nEpoch {epoch + 1} / {num_epochs}:')
+        LOG.debug(f'Train Loss: {avg_train_loss:.3f}, Train Acc: {train_accuracy:.2f}%')
 
-        # Save best model
-        if val_accuracy > best_val_acc:
-            best_val_acc = val_accuracy
-            torch.save({
-                'epoch': epoch,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'val_acc': best_val_acc
-            }, 'LSTM_best_temp.pth')
+    # Testing phase - switch model to evaluation mode
+    model.eval()
 
-        early_stopping(val_loss)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            break
+    test_loss = 0.0
+    test_correct_predictions = 0
+    test_total_predictions = 0
+
+    all_labels = []
+    all_predictions = []
+
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs.permute(0, 2, 1))
+
+            # Loss calculation
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+
+            # Get the predictions
+            _, predicted = torch.max(outputs, 1)
+
+            # Collect all the predictions made 
+            all_labels.extend(labels.cpu().numpy())
+            all_predictions.extend(predicted.cpu().numpy())
+
+    # Average loss and accuracy:
+    test_total_predictions = len(all_labels)
+    test_correct_predictions = sum([1 for i in range(test_total_predictions) if all_labels[i] == all_predictions[i]])
+
+    avg_test_loss = test_loss / len(test_loader)
+    test_accuracy = 100 * test_correct_predictions / test_total_predictions
+
+    # Addional metrics: precision, f1-score, confusion matrix
+    precision = precision_score(all_labels, all_predictions, average='macro')
+    f1 = f1_score(all_labels, all_predictions, average='macro')
+    conf_matrix = confusion_matrix(all_labels, all_predictions)
+
+    history['test_loss'].append(avg_test_loss)
+    history['test_acc'].append(test_accuracy)
+    history['precision'].append(precision)
+    history['f1'].append(f1)
+    history['conf_matrix'].append(conf_matrix)
+
+    # Save model
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+    }, 'LSTM_best.pth')
     
-        scheduler.step(avg_val_loss)
-        current_lr = optimizer.param_groups[0]['lr']
-        history['lr'].append(current_lr)
-    
-    return history, best_val_acc
+    return history
 
 
 if __name__ == "__main__":
+    configure_logging(logging.DEBUG)
+    
     device = get_system_device()
     
     # Get data with k-fold splits
-    print("Loading dataset...")
+    LOG.debug("Loading dataset...")
 
     # NOTE - can produce by running "python dataset_bci_iv_2a/dataset.py 1 100 90"
     df = pd.read_parquet("dataset_bci_iv_2a/A01_100_90.parquet", engine="pyarrow")
 
-    print(f"DF Shape: {df.shape}")
-    print(df.head(3))
+    LOG.debug(f"DF Shape: {df.shape}")
+    LOG.debug(df.head(3))
 
-    labels: pd.Series = df["Label"]
-    features: pd.DataFrame = df.drop(columns=["Label"])
+    train, test = train_test_split(df, test_size=0.2, random_state=42)
 
-    # Gets indicies to split the data into k-folds
-    # See: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.KFold.html
-    kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+    train_labels = train["Label"]
+    train_features = train.drop(columns=["Label"])
 
-    # Store the results from k-fold cross validation:
-    fold_accuracies = []
-    best_fold_acc = 0.0
-    best_fold = 0
+    test_labels = test["Label"]
+    test_features = test.drop(columns=["Label"])
 
-    for fold_index, (train_idx, val_idx) in enumerate(kfold.split(features)):
-        print(f"Fold {fold_index + 1}")
+    LOG.debug(f"Creating datasets...")
 
-        # Clear GPU memory before each fold
-        if "cuda" in device.type:
-            torch.cuda.empty_cache()
-        
-        elif "xpu" in device.type:
-            torch.xpu.empty_cache()
+    train_dataset = BciIvDataset(train_features, train_labels)
+    test_dataset = BciIvDataset(test_features, test_labels)
 
-        print(f"Creating datasets for fold {fold_index + 1}...")
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
-        train_dataset = BciIvDataset(features.iloc[train_idx], labels.iloc[train_idx])
-        val_dataset = BciIvDataset(features.iloc[val_idx], labels.iloc[val_idx])
+    LOG.debug(f"Initializing model...")
 
-        train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-        val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False)
+    model = LSTM().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.00005)
 
-        print(f"Initializing model for fold {fold_index + 1}...")
+    LOG.debug(f"Training...")
 
-        model = LSTM().to(device)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.00005)
-        
-        # See: https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.ReduceLROnPlateau.html#reducelronplateau
-        scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-            optimizer,
-            mode='min',
-            patience=3,
-            factor=0.1,
-            min_lr=1e-6
-        )
+    # Clear GPU memory
+    if "cuda" in device.type:
+        torch.cuda.empty_cache()
+    
+    elif "xpu" in device.type:
+        torch.xpu.empty_cache()
 
-        print(f"Training fold {fold_index + 1}...")
-
-        history, fold_val_acc = train_model(
+    try:
+        history = train_model(
             model,
             train_loader,
-            val_loader,
+            test_loader,
             criterion,
             optimizer,
-            scheduler,
             device
         )
-        fold_accuracies.append(fold_val_acc)
 
-        print(f"Fold {fold_index + 1} validation accuracy: {fold_val_acc:.2f}")
+    except Exception as e:
+        LOG.error(f"Caught exception: {e}")
+        exit(1)
 
-        print(f"Saving results for fold {fold_index + 1}...")
-        np.save(f"LSTM_fold_{fold_index + 1}_history.npy", history)
+    LOG.debug(f"Test Loss: {history['test_loss'][-1]:.3f}, Test Acc: {history['test_acc'][-1]:.2f}%")
 
-        if fold_val_acc > best_fold_acc:
-            best_fold_acc = fold_val_acc
-            best_fold = fold_index + 1
-
-            if os.path.exists("LSTM_best_temp.pth"):
-                os.replace("LSTM_best_temp.pth", f"LSTM_best_fold_{best_fold}.pth")
-
-        else:
-            # Remove temporary model if its not the best
-            if os.path.exists("LSTM_best_temp.pth"):
-                os.remove("LSTM_best_temp.pth")
-
-        # Clear model from GPU:
-        del model
-
-    print(f"5-Fold Cross Validation Results:")
-    print(f"Average Validation Accuracy: {np.mean(fold_accuracies):.2f}")
-    print(f"S.D. of Validation Accuracies: {np.std(fold_accuracies):.2f}")
-    print(f"Individual Fold Accuracies: {[f'Fold: {fold + 1}: {acc:.2f}' for fold, acc in enumerate(fold_accuracies)]}")
-    print(f"Best model saved from fold {best_fold} with accuracy: {best_fold_acc:.2f}")
+    np.save(f"LSTM_history.npy", history)
--- result.py
+++ result.py
@@ -1,8 +1,41 @@
 import argparse
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+
+
+def plot_train_test_split_label_distribution() -> None:
+    df = pd.read_parquet("dataset_bci_iv_2a/A01_100_90.parquet", engine="pyarrow")
+
+    train, test = train_test_split(df, test_size=0.2, random_state=42)
+
+    # Visualize the distribution of the "Label" column values in the train set
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    train_counts = train['Label'].value_counts().sort_index()
+    plt.bar(train_counts.index, train_counts.values)
+    plt.title('Train Set Label Distribution')
+    plt.xlabel('Label')
+    plt.ylabel('Count')
+
+    # Visualize the distribution of the "Label" column values in the test set
+    plt.subplot(1, 2, 2)
+    test_counts = test['Label'].value_counts().sort_index()
+    plt.bar(test_counts.index, test_counts.values)
+    plt.title('Test Set Label Distribution')
+    plt.xlabel('Label')
+    plt.ylabel('Count')
+
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == "__main__":
+    plot_train_test_split_label_distribution()
+    
     parser = argparse.ArgumentParser(description="View model results saved in .npy files.")
     parser.add_argument("file", type=str, help="Path to .npy file.")
     args = parser.parse_args()
@@ -13,4 +46,5 @@ if __name__ == "__main__":
     print(history.keys())
 
     # Example - show the confusion matrix of the last epoch
-    print(history['val_conf_matrix'][-1])
+    print(history['conf_matrix'][-1])
+    
